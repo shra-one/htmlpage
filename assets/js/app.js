17055f53d627f8785b8e951b972a6b607dd0b633
shrawan <shrawankumarshrestha@gmail.com>
2022-07-20 11:19:17 +0545

    slick slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,50 @@
-var user ="Shrawan";
-var fullName=`My name is ${user}`
+$(document).ready(function(){
+   // slider code goes here
+   $('#js-hero-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+   //  arrows:false,
+    appendArrows:"#hero-nav",
+    prevArrow:"<button class='slick-arrow'><i class='fa fa-angle-left'></i></button>",
+    nextArrow:"<button class='slick-arrow'><i class='fa fa-angle-right'></i></button>",
+   //  speed: 500,
+   //  fade: true,
+   //  cssEase: 'linear'
+ 
+  });
+
+  //testimonial slider
+  $('#js-testnimonial-slider').slick({
+   dots: true,
+   infinite: false,
+   speed: 300,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   mobileFirst:true,
+   responsive: [
+     {
+       breakpoint: 1024,
+       settings: {
+         slidesToShow: 4,
+         slidesToScroll: 2,
+        }
+     },
+     {
+       breakpoint: 600,
+       settings: {
+         slidesToShow: 2,
+        
+       }
+     },
+     
+     // You can unslick at a given breakpoint now by adding:
+     // settings: "unslick"
+     // instead of a settings object
+   ]
+  })
+  });
+
+  $(function(){
+     // slider code goes here
+  })
